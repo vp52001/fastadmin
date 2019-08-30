3be4ed856a29c1ded8ae15f6f63010b650376b75
qinzhixiang <897333949@qq.com>
2019-08-30 16:51:20 +0800

    关闭验证码

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -74,11 +74,11 @@ class Index extends Backend
                 'password'  => $password,
                 '__token__' => $token,
             ];
-            if (Config::get('fastadmin.login_captcha')) {
-                $rule['captcha'] = 'require|captcha';
-                $data['captcha'] = $this->request->post('captcha');
-            }
-            $validate = new Validate($rule, [], ['username' => __('Username'), 'password' => __('Password'), 'captcha' => __('Captcha')]);
+//            if (Config::get('fastadmin.login_captcha')) {
+//                $rule['captcha'] = 'require|captcha';
+//                $data['captcha'] = $this->request->post('captcha');
+//            }
+//            $validate = new Validate($rule, [], ['username' => __('Username'), 'password' => __('Password'), 'captcha' => __('Captcha')]);
             $result = $validate->check($data);
             if (!$result) {
                 $this->error($validate->getError(), $url, ['token' => $this->request->token()]);
